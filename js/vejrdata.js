a0b9f4de1997750de2d1829acf2a98aa8b0a237d
Alexander Grydgaard <alex043t@edu.easj.dk>
2021-12-02 11:44:33 +0100

    vejrdata opdateret (Note: url ikke opdateret i js metoderne endnu)

--- js/vejrdata.js
+++ js/vejrdata.js
@@ -47,7 +47,7 @@ Vue.createApp({
         },
         async getTempData() {
             const current = new Date();
-            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-"+`${current.getDate()-1}`+"T00:00:00Z/"+`${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-"+`${current.getDate()}`+"T00:00:00Z&"+"parameterId=temp_mean_past1h"
+            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-0"+`${current.getDate()-1}`+"T00:00:00Z/"+`${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-0"+`${current.getDate()}`+"T00:00:00Z&"+"parameterId=temp_mean_past1h"
             try {
                 const response = await axios.get(url)
                 this.vejrDataer = await response.data
@@ -57,7 +57,7 @@ Vue.createApp({
         },
         async getRegnData() {
             const current = new Date();
-            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-"+`${current.getDate()-1}`+"T00:00:00Z/"+`${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-"+`${current.getDate()}`+"T00:00:00Z&"+"parameterId=precip_past1h"
+            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-0"+`${current.getDate()-1}`+"T00:00:00Z/"+`${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-0"+`${current.getDate()}`+"T00:00:00Z&"+"parameterId=precip_past1h"
             try {
                 const response = await axios.get(url)
                 this.regnData = await response.data
@@ -67,7 +67,7 @@ Vue.createApp({
         },
         async getVindData() {
             const current = new Date();
-            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-"+`${current.getDate()-1}`+"T00:00:00Z/"+`${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-"+`${current.getDate()}`+"T00:00:00Z&"+"parameterId=wind_speed"
+            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-0"+`${current.getDate()-1}`+"T00:00:00Z/"+`${current.getFullYear()}`+"-"+`${current.getMonth()+1}`+"-0"+`${current.getDate()}`+"T00:00:00Z&"+"parameterId=wind_speed"
             try {
                 const response = await axios.get(url)
                 this.vindData = await response.data
