b2c046ccc27d745cc57d435c52b4fa42dea529e7
Emil Wilhelm Winther <emil377g@edu.easj.dk>
2021-12-07 13:32:02 +0100

    statistics

--- js/vejrstatistik.js
+++ js/vejrstatistik.js
@@ -1,3 +1,38 @@
+google.charts.load('current', { 'packages': ['bar'] });
+google.charts.setOnLoadCallback(drawChart);
+//not vue
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Dag', 'Parkerede biler', 'Nedbør'],
+        ['07/12-2021', 64, 3]
+    ]);
+
+    var options = {
+        chart: {
+            title: 'Statistik over parkeringspladser og vejr',
+            subtitle: 'Statistik',
+        },
+        bars: 'vertical',
+        vAxis: { format: 'decimal' },
+        height: 400,
+        colors: ['#1b9e77', '#d95f02', '#7570b3']
+    };
+
+    var chart = new google.charts.Bar(document.getElementById('chart_div'));
+
+    chart.draw(data, google.charts.Bar.convertOptions(options));
+
+    var btns = document.getElementById('btn-group');
+
+    btns.onclick = function (e) {
+
+        if (e.target.tagName === 'BUTTON') {
+            options.vAxis.format = e.target.id === 'none' ? '' : e.target.id;
+            chart.draw(data, google.charts.Bar.convertOptions(options));
+        }
+    }
+}
+//not vue
 Vue.createApp({
     data() {
         return {
@@ -10,22 +45,26 @@ Vue.createApp({
             dayDate: null,
             beforeDate: null,
             monthDate: null,
-            beforeMonth:null,
+            beforeMonth: null,
         }
     },
     async created() {
         try {
-            
-            
+
+
         } catch (ex) {
             alert(ex.message)
         }
     },
+    mounted: function()
+    {
+
+    },
     methods: {
         async getTempData() {
             const current = new Date(this.selectDate);
-            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${this.selectDate}`+"T00:00:00Z/"+`${this.selectDate}`+"T23:59:59Z&"+"parameterId=temp_mean_past1h"
-            const url2 = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${this.selectDate2}`+"T00:00:00Z/"+`${this.selectDate2}`+"T23:59:59Z&"+"parameterId=temp_mean_past1h"
+            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${this.selectDate}` + "T00:00:00Z/" + `${this.selectDate}` + "T23:59:59Z&" + "parameterId=temp_mean_past1h"
+            const url2 = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${this.selectDate2}` + "T00:00:00Z/" + `${this.selectDate2}` + "T23:59:59Z&" + "parameterId=temp_mean_past1h"
             try {
                 const response = await axios.get(url)
                 this.tempData1 = await response.data
@@ -35,10 +74,11 @@ Vue.createApp({
             } catch (ex) {
                 alert(ex.message)
             }
+            
         },
         async getRegnData() {
             const current = new Date();
-            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${current.getFullYear()}`+"-"+`${this.monthDate-1}`+"-"+`${this.beforeDate}`+"T00:00:00Z/"+`${current.getFullYear()}`+"-"+`${this.monthDate}`+"-"+`${this.dayDate}`+"T00:00:00Z&"+"parameterId=precip_past1h"
+            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${current.getFullYear()}` + "-" + `${this.monthDate - 1}` + "-" + `${this.beforeDate}` + "T00:00:00Z/" + `${current.getFullYear()}` + "-" + `${this.monthDate}` + "-" + `${this.dayDate}` + "T00:00:00Z&" + "parameterId=precip_past1h"
             try {
                 const response = await axios.get(url)
                 this.regnData = await response.data
@@ -48,7 +88,7 @@ Vue.createApp({
         },
         async getVindData() {
             const current = new Date();
-            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${current.getFullYear()}`+"-"+`${this.monthDate-1}`+"-"+`${this.beforeDate}`+"T00:00:00Z/"+`${current.getFullYear()}`+"-"+`${this.monthDate}`+"-"+`${this.dayDate}`+"T00:00:00Z&"+"parameterId=wind_speed"
+            const url = "https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=9c03456a-00ce-48db-a13b-907255c2eb73&stationId=06184&" + `datetime=${current.getFullYear()}` + "-" + `${this.monthDate - 1}` + "-" + `${this.beforeDate}` + "T00:00:00Z/" + `${current.getFullYear()}` + "-" + `${this.monthDate}` + "-" + `${this.dayDate}` + "T00:00:00Z&" + "parameterId=wind_speed"
             try {
                 const response = await axios.get(url)
                 this.vindData = await response.data
